944ea7037d0e44e47aa18cf5d69a66de083017f3
Marcos Ruueda Jimenez <marcosruedajimenez@gmail.com>
2020-10-04 12:30:52 +0200

    marcos 2

--- DBImages.py
+++ DBImages.py
@@ -1,51 +1,19 @@
-# importing google_images_download module 
-from google_images_download import google_images_download  
-  
-# creating object 
-response = google_images_download.googleimagesdownload()  
-  
-search_queries = [ 
-    'New York in winter'
-    ] 
-  
-  
-def downloadimages(query): 
-    # keywords is the search query 
-    # format is the image file format 
-    # limit is the number of images to be downloaded 
-    # print urs is to print the image file url 
-    # size is the image size which can 
-    # be specified manually ("large, medium, icon") 
-    # aspect ratio denotes the height width ratio 
-    # of images to download. ("tall, square, wide, panoramic") 
-    arguments = {"keywords": query, 
-                 "format": "jpg", 
-                 "limit":3, 
-                 "print_urls":True, 
-                 "size": "medium", 
-                 "aspect_ratio":"panoramic"} 
-    try: 
-        response.download(arguments) 
-      
-    # Handling File NotFound Error     
-    except FileNotFoundError:  
-        print("error 1")
-        arguments = {"keywords": query, 
-                     "format": "jpg", 
-                     "limit":2, 
-                     "print_urls":True,  
-                     "size": "medium"} 
-                       
-        # Providing arguments for the searched query 
-        try: 
-            # Downloading the photos based 
-            # on the given arguments 
-            response.download(arguments)  
-        except: 
-            print("error 2")
-            pass
-  
-# Driver Code 
-for query in search_queries: 
-    downloadimages(query)  
-    print("finished")  
+from simple_image_download import simple_image_download as simp
+from DBinterface import nasaDBinterface
+
+cities_list = ['New york','Pasadena','Las Vegas','Los Angeles']
+interfase = nasaDBinterface()
+city = get_random_city()
+
+response = simp.simple_image_download
+
+
+response().download( city , 5)
+
+
+#print(response().urls( ciudad , 5))
+
+def get_random_city():
+    cities_list = -....Msnurl
+
+    city = cities_list[random.randint(0, len(cities_list)-1)]
